1e9cf5c5d427d8eda0b4e6b81cda64b8037b11f5
mukeshkr03 <mukeshkr0313@gmail.com>
2023-07-25 15:27:39 +0530

    Updates

---
+++ precache-manifest.1f99b48a6d89b4ed91cd814b5b6b1476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "717c2cbee33f9af838a2",
+    "url": "/color-picker/static/css/main.a2168b6f.chunk.css"
+  },
+  {
+    "revision": "717c2cbee33f9af838a2",
+    "url": "/color-picker/static/js/main.54180610.chunk.js"
+  },
+  {
+    "revision": "a7d8e61640eb3f8830d1",
+    "url": "/color-picker/static/js/runtime~main.3187d74d.js"
+  },
+  {
+    "revision": "ceb3bf39589a7db2ccf8",
+    "url": "/color-picker/static/css/2.e3c79e34.chunk.css"
+  },
+  {
+    "revision": "ceb3bf39589a7db2ccf8",
+    "url": "/color-picker/static/js/2.ae2cc8e0.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-picker/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "ac5f125231974f8ab7db6276903c15cd",
+    "url": "/color-picker/index.html"
+  }
+];
